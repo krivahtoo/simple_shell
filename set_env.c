9e7a4d29df692e6ab314ed274f0a2d1d9e3c8e4c
Dibuji <emekzy10@gmail.com>
2023-05-17 12:25:41 +0000

    set env implementation

---
+++ set_env.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+extern char **environ;
+
+/**
+ *
+ */
+
+int setenv(const char *name, const char *value, int overwrite)
+{
+	char *env_value, *temp, new_entry, new_environ;
+	int i, num_entries = 0;
+	
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		env_value = strdup(environ[i]);
+		temp = strtok(env_value, "=");
+
+		if (strcmp(temp, name) == 0)
+		{
+			if (overwrite != 0)
+			{
+				new_entry = malloc(strlen(name) + strlen(value) + 2);
+				new_entry = strcpy(new_entry, name);
+				new_entry = strcpy(new_entry, "=");
+				new_entry = strcpy(new_entry, value);
+				environ[i] = new_entry;
+			}
+			free(env_value);
+			return (0);
+		}
+	}
+
+		new_entry = malloc(strlen(name) + strlen(value) + 2);
+		new_entry = strcpy(new_entry, name);
+		new_entry = strcpy(new_entry, "=");
+		new_entry = strcpy(new_entry, value);
+
+			/* Find out how many entries are in environ */
+		while (environ[num_entries] != NULL)
+			num_entries++;
+
+		new_environ = realloc(environ, (num_entries + 2));
+
+		if (new_environ == NULL)
+		{
+			free(new_entry);
+			return (-1);
+		}
+
+		new_environ[num_entries] = new_entry;
+		new_environ[num_entries + 1] = NULL;
+		environ = new_environ;
+
+		return (0);
+}
+
