15c37b0b97072e1b4438bdd4f4348fbf44496e67
Noah Too <krivahtoo@gmail.com>
2023-05-26 15:05:29 +0300

    replace stdlib functions with our implementations

--- env.c
+++ env.c
@@ -14,20 +14,20 @@
 int _setenv(const char *name, const char *value, int overwrite)
 {
 	char *env_value, *temp, *new_entry, **new_environ;
-	int i, num_entries = 0;
+	int i, num_entries = 0, len;
 
 	for (i = 0; environ[i] != NULL; i++)
 	{
-		env_value = strdup(environ[i]);
+		env_value = _strdup(environ[i]);
 		temp = strtok(env_value, "=");
-		if (strcmp(temp, name) == 0)
+		if (_strncmp(temp, name, _strlen(name)) == 0)
 		{
 			if (overwrite != 0)
 			{
-				new_entry = malloc(strlen(name) + strlen(value) + 2);
-				new_entry = strcat(new_entry, name);
-				new_entry = strcat(new_entry, "=");
-				new_entry = strcat(new_entry, value);
+				new_entry = malloc(_strlen(name) + _strlen(value) + 2);
+				new_entry = _strcat(new_entry, name);
+				new_entry = _strcat(new_entry, "=");
+				new_entry = _strcat(new_entry, value);
 				environ[i] = new_entry;
 			}
 			free(env_value);
@@ -35,14 +35,15 @@ int _setenv(const char *name, const char *value, int overwrite)
 		}
 	}
 
-	new_entry = malloc(strlen(name) + strlen(value) + 2);
-	new_entry = strcat(new_entry, name);
-	new_entry = strcat(new_entry, "=");
-	new_entry = strcat(new_entry, value);
+	new_entry = malloc(_strlen(name) + _strlen(value) + 2);
+	new_entry = _strcat(new_entry, name);
+	new_entry = _strcat(new_entry, "=");
+	new_entry = _strcat(new_entry, value);
 	/* Find out how many entries are in environ */
 	while (environ[num_entries] != NULL)
 		num_entries++;
-	new_environ = (char **)realloc(environ, (num_entries + 2) * sizeof(char *));
+	len = (num_entries + 1) * sizeof(char *);
+	new_environ = (char **)_realloc(environ, len, len + i);
 	if (new_environ == NULL)
 	{
 		free(new_entry);
@@ -68,7 +69,7 @@ char *_getenv(const char *name)
 	/* Extracts the PATH variable in environ */
 	while (*env)
 	{
-		if (strncmp(*env, name, 4) == 0)
+		if (_strncmp(*env, name, _strlen(name)) == 0)
 		{
 			ptr = *env;
 			while (*ptr != '=' && *ptr != '\0')
